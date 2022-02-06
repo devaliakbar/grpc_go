1b4060911ec4a224c0eccfd420c61b3cb7bb407b
Ali <dev.aliakbar@gmail.com>
2022-02-06 18:05:12 +0530

    cleaned server start code in blog

---
+++ blog/server/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"blog/blogpb"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	blogpb.UnimplementedBlogServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	blogpb.RegisterBlogServiceServer(s, &server{})
+	reflection.Register(s)
+
+	go func() {
+		log.Println("Starting server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	///Waiting for interrupt
+	<-ch
+	log.Println("Stopping the server")
+	s.Stop()
+	log.Println("Closing the listener")
+	lis.Close()
+}
