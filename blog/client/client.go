df45d86f2a00c8ac7904b62a3b877614a2b9fc8e
Ali <dev.aliakbar@gmail.com>
2022-02-06 20:22:41 +0530

    Create Blog

---
+++ blog/client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"blog/blogpb"
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to dial: %s", err)
+	}
+
+	defer cc.Close()
+
+	c := blogpb.NewBlogServiceClient(cc)
+
+	//Create Blog
+	blog := &blogpb.Blog{
+		AuthorId: "x The Author",
+		Title:    "Title X",
+		Content:  "Content X",
+	}
+
+	res, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{
+		Blog: blog,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create: %s", err)
+	}
+
+	log.Printf("Blog created: %s", res)
+}
--- blog/server/server.go
+++ blog/server/server.go
@@ -2,20 +2,82 @@ package main
 
 import (
 	"blog/blogpb"
+	"context"
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
+
+var collection *mongo.Collection
 
 type server struct {
 	blogpb.UnimplementedBlogServiceServer
 }
 
+type blogType struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorId string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
+
+func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
+	log.Printf("Create Blog request: %s", req)
+
+	blog := req.GetBlog()
+
+	data := blogType{
+		AuthorId: blog.GetAuthorId(),
+		Title:    blog.GetTitle(),
+		Content:  blog.GetContent(),
+	}
+
+	res, err := collection.InsertOne(context.Background(), data)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Internal error: %s", err),
+		)
+	}
+
+	oid, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Connot convert objectId",
+		)
+	}
+
+	return &blogpb.CreateBlogResponse{
+		Blog: &blogpb.Blog{
+			Id:       oid.Hex(),
+			AuthorId: blog.GetAuthorId(),
+			Title:    blog.GetTitle(),
+			Content:  blog.GetContent(),
+		},
+	}, nil
+}
+
 func main() {
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Connecting to MongoDb")
+	collection = client.Database("gprc-blogdb").Collection("blog")
+
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
@@ -40,4 +102,6 @@ func main() {
 	s.Stop()
 	log.Println("Closing the listener")
 	lis.Close()
+	log.Println("Closing MongoDb connection")
+	client.Disconnect(context.TODO())
 }
